c6360cf633c2bb613c108df234fc8c675d438627
patr0nus <patronum@outlook.com>
2015-01-13 10:56:10 +0800

    Initial Commit

---
+++ src/extract_srts.py
@@ -0,0 +1,29 @@
+from os import listdir, mkdir
+from os.path import isfile, join
+import rarfile
+import chardet
+
+if __name__ == '__main__':
+  rarpath = 'rars'
+  targetpath = 'srts'
+  rarfiles = (f for f in listdir(rarpath))
+  
+  for file in rarfiles:
+    print file
+    dest = None
+
+    try:
+      with rarfile.RarFile(join(rarpath, file)) as rar:
+        if rar.needs_password():
+          print 'Needs passwords..'
+          continue
+        for f in rar.infolist():
+          if f.filename.endswith('.srt') and not f.isdir():
+            if dest is None:
+              dest = join(targetpath, file)
+              mkdir(dest)
+            rar.extract(f, dest)
+            
+    except Exception as e:
+      print 'ERROR:', e
+  
